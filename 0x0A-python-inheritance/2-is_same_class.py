7a31e5f67ba0459697a963187e1d24115d45e8ce
nat <natadjadeh@gmail.com>
2023-02-06 13:52:39 -0800

    2-is_same_class correction

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,9 +2,6 @@
 
 
 def is_same_class(obj, a_class):
-    """'''function: is_same_class
-    obj: an object
-    a_class: a class
-    Returns: Bool
-    """
-    return type(obj) == a_class
+    """Method that return True if an object is an instance of a class"""
+
+    return type(obj) is a_class
