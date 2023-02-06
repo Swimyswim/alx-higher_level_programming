8c682a228c1f9c2ff3c98cc234764e0e0b56a85d
nat <natadjadeh@gmail.com>
2023-02-06 14:44:21 -0800

    Further commit 9

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 """
-===================================
-module with class BaseGeometry
-===================================
+more class base
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+"""
+Square class
 """
 
 
 class Square(Rectangle):
-    """Square class that inherits from Rectangle that inherits BaseGeometry"""
-
+    """ Square Class """
     def __init__(self, size):
-        """Method for initialized the attrubutes"""
-
-        super().__init__(size, size)
-        self.integer_validator("size", size)
+        """ size init"""
         self.__size = size
-
-    def area(self):
-        """rectangle area"""
-
-        return self.__size ** 2
+        super().__init__(self.__size, self.__size)
 
     def __str__(self):
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
