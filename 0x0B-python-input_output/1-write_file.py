e9f40757e45124d0dd9dcda0e848b759ff076e3a
nat <natadjadeh@gmail.com>
2023-02-07 09:29:04 -0800

    Commit 2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-"""
-function that writes a string to a text file (UTF8) and returns the number of characters written
-"""
-
-
 def write_file(filename="", text=""):
-    """ module write_file
-    """
-    with open(filename, 'w') as f:
+    """ function that writes a string to a text file (UTF8)  """
+
+    with open(filename, "w", encoding='utf-8') as f:
         return f.write(text)
